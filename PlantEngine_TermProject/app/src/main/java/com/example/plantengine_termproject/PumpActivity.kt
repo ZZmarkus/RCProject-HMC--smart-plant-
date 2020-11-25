bd4cb65b576bf5fb2f9b10af9d87cd568bd4b507
HMC <1170313@naver.com>
2020-11-26 03:07:44 +0900

    UI수정

--- PlantEngine_TermProject/app/src/main/java/com/example/plantengine_termproject/PumpActivity.kt
+++ PlantEngine_TermProject/app/src/main/java/com/example/plantengine_termproject/PumpActivity.kt
@@ -1,5 +1,6 @@
 package com.example.plantengine_termproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -8,6 +9,7 @@ import android.view.View
 import android.widget.SeekBar
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_pump.*
+import kotlinx.android.synthetic.main.activity_water_tank.*
 import java.lang.Thread.sleep
 import java.util.*
 import kotlin.concurrent.schedule
@@ -21,8 +23,15 @@ class PumpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pump)
 
-
-
-
+        ClickLister();
+    }
+    private fun ClickLister(){
+        home3.setOnClickListener {
+            finish()
+        }
+        setting3.setOnClickListener {
+            val intent: Intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
